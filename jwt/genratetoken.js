267ffa5944589ade23c9f36831ba6256a9dbf766
RAHMOUNI Mohamed Amine <144079280+amine-rhm@users.noreply.github.com>
2024-05-12 19:07:31 +0200

    Update genratetoken.js

--- jwt/genratetoken.js
+++ jwt/genratetoken.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const generateJwt = (userId) => {
-    const token = jwt.sign(userId, process.env.jwtSecret, { expiresIn: "1h" });
-    return "Bearer " + token; 
+    const token = jwt.sign( userId, process.env.jwtSecret, { expiresIn: "1h" });
+    return token;
 };
 
 module.exports = { generateJwt };
